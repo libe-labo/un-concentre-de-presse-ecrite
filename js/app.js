f5621862a166aee7aa5e668dc8c0af8d796273a7
Paul Joannon <hello@pauljoannon.com>
2015-05-27 17:39:42 +0200

    retrieve data and format it

--- js/app.js
+++ js/app.js
@@ -2,5 +2,25 @@
 
 var app = angular.module('app', ['lheader']);
 
-app.controller('Ctrl', ['$scope', function($scope) {
+app.controller('Ctrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('data/data.csv').then(function(response) {
+        var data = {};
+
+        var csvArray = CSVToArray(response.data);
+        var csvHeader = _.invert(_.first(csvArray.splice(0, 1)));
+
+        for (var i = 0; i < csvArray.length; ++i) {
+            var title = csvArray[i][csvHeader.Titre];
+            var year = csvArray[i][csvHeader['Année']];
+            var group = csvArray[i][csvHeader.Groupe];
+            if (data[title] == null) {
+                data[title] = {
+                    name : title
+                };
+            }
+            data[title][year] = group;
+        }
+
+        $scope.data = _.values(data);
+    });
 }]);
